975c235e5fc870d5789e3cdc4b5e9eb8db325c4d
samuel-pratt <sjpratt19@gmail.com>
2021-12-12 19:56:31 -0800

    Added sentry monitoring

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.16
 
 require (
 	github.com/PuerkitoBio/goquery v1.7.1
+	github.com/getsentry/sentry-go v0.11.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/robfig/cron v1.2.0
 	github.com/tidwall/gjson v1.8.1
--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
+	"github.com/getsentry/sentry-go"
+	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
 	"github.com/robfig/cron"
 	"github.com/tidwall/gjson"
@@ -19,7 +22,7 @@ func updateSchedule() {
 	jsonString, _ := json.Marshal(response)
 	err := ioutil.WriteFile("sailings.json", []byte(jsonString), 0644)
 	if err != nil {
-		fmt.Println(err)
+		sentry.CaptureException(err)
 	}
 	fmt.Print("Updated sailings.json at: ")
 	fmt.Println(time.Now())
@@ -28,8 +31,10 @@ func updateSchedule() {
 func getAll(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	data, err := ioutil.ReadFile("sailings.json")
 	if err != nil {
-		fmt.Println(err)
+		sentry.CaptureException(err)
 	}
+
+	sentry.CaptureMessage("REQUEST: getAll()")
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(data)
@@ -38,8 +43,10 @@ func getAll(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 func getDepartureTerminal(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	data, err := ioutil.ReadFile("sailings.json")
 	if err != nil {
-		fmt.Println(err)
+		sentry.CaptureException(err)
 	}
+
+	sentry.CaptureMessage("REQUEST: getDepartureTerminal()")
 
 	departureTerminals := [6]string{
 		"Tsawwassen",
@@ -67,8 +74,10 @@ func getDepartureTerminal(w http.ResponseWriter, r *http.Request, ps httprouter.
 func getDestinationTerminal(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	data, err := ioutil.ReadFile("sailings.json")
 	if err != nil {
-		fmt.Println(err)
+		sentry.CaptureException(err)
 	}
+
+	sentry.CaptureMessage("REQUEST: getDestinationTerminal()")
 
 	departureTerminals := [6]string{
 		"Tsawwassen",
@@ -106,14 +115,30 @@ func getDestinationTerminal(w http.ResponseWriter, r *http.Request, ps httproute
 }
 
 func main() {
-	// Schedule update every hour
+	// dotenv setup
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Setup sentry
+	err = sentry.Init(sentry.ClientOptions{
+		Dsn: os.Getenv("SENTRY_DSN"),
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	// Flush buffered events before the program terminates.
+	defer sentry.Flush(2 * time.Second)
+
+	// Schedule update every minute
 	c := cron.New()
 	c.AddFunc("@every 1m", updateSchedule)
 	c.Start()
 
+	// Router setup
 	router := httprouter.New()
 
-	// Root api call
 	router.GET("/api/", getAll)
 	router.GET("/api/:departureTerminal/", getDepartureTerminal)
 	router.GET("/api/:departureTerminal/:destinationTerminal/", getDestinationTerminal)
