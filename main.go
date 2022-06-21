5b2084f2392f8f96d5cb94b233aa095c5db7c021
samuel-pratt <sjpratt19@gmail.com>
2022-06-21 15:22:04 -0700

    Removed default port number

--- main.go
+++ main.go
@@ -120,9 +120,5 @@ func main() {
 	router.NotFound = http.FileServer(http.Dir("./static"))
 
 	port := os.Getenv("PORT")
-	if port == "" {
-		port = "9000" // Default port if not specified
-	}
-
 	http.ListenAndServe(":"+port, router)
 }
