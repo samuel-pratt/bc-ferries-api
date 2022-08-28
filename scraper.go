e81c08f2e90c5373edda97ae2b41fbaba6762145
samuel-pratt <sjpratt19@gmail.com>
2022-08-28 09:18:19 -0700

    Fixed full ferry showing 0

--- scraper.go
+++ scraper.go
@@ -146,7 +146,7 @@ func ScrapeCapacityRoutes() Response {
 
 					// FILL
 					fill := strings.TrimSpace(sailingData.Find(".cc-percentage").First().Text())
-					if fill == "Full" {
+					if fill == "Full" || fill == "100%" {
 						sailing.Fill = 100
 					} else {
 						fill, err := strconv.Atoi(strings.Split(fill, "%")[0])
